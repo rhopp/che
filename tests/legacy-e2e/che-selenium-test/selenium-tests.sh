fb0c35f3d32fa3acefa11d1158425ef8a01020f9
Radim Hopp <rhopp@redhat.com>
2019-12-16 16:33:24 +0100

    donotmerge

--- tests/legacy-e2e/che-selenium-test/selenium-tests.sh
+++ tests/legacy-e2e/che-selenium-test/selenium-tests.sh
@@ -10,33 +10,37 @@
 # Contributors:
 #   Red Hat, Inc. - initial API and implementation
 #
-export CUR_DIR=$(cd "$(dirname "$0")"; pwd)
-export CALLER=$(basename $0)
 
-cd $CUR_DIR
 
-TESTS_SCOPE="--suite=CheSuite.xml"
-CLEAN_GOAL="clean"
-for var in "$@"; do
-    if [[ "$var" =~ --test=.* ]] || [[ "$var" =~ --suite=.* ]]; then
-        TESTS_SCOPE=
-        break
-    fi
+sudo cat /opt/ci-umount.sh
 
-    if [[ "$var" == "--compare-with-ci" ]] \
-        || [[ "$var" == "--failed-tests" ]] \
-        || [[ "$var" == "--regression-tests" ]]; then
-        TESTS_SCOPE=
-        CLEAN_GOAL=
-        break
-    fi
-done
+# export CUR_DIR=$(cd "$(dirname "$0")"; pwd)
+# export CALLER=$(basename $0)
 
-mvn $CLEAN_GOAL dependency:unpack-dependencies \
-    -DincludeArtifactIds=che-selenium-core \
-    -DincludeGroupIds=org.eclipse.che.selenium \
-    -Dmdep.unpack.includes=webdriver.sh \
-    -DoutputDirectory=${CUR_DIR}/target/bin
-chmod +x target/bin/webdriver.sh
+# cd $CUR_DIR
 
-(target/bin/webdriver.sh "$TESTS_SCOPE" $@)
+# TESTS_SCOPE="--suite=CheSuite.xml"
+# CLEAN_GOAL="clean"
+# for var in "$@"; do
+#     if [[ "$var" =~ --test=.* ]] || [[ "$var" =~ --suite=.* ]]; then
+#         TESTS_SCOPE=
+#         break
+#     fi
+
+#     if [[ "$var" == "--compare-with-ci" ]] \
+#         || [[ "$var" == "--failed-tests" ]] \
+#         || [[ "$var" == "--regression-tests" ]]; then
+#         TESTS_SCOPE=
+#         CLEAN_GOAL=
+#         break
+#     fi
+# done
+
+# mvn $CLEAN_GOAL dependency:unpack-dependencies \
+#     -DincludeArtifactIds=che-selenium-core \
+#     -DincludeGroupIds=org.eclipse.che.selenium \
+#     -Dmdep.unpack.includes=webdriver.sh \
+#     -DoutputDirectory=${CUR_DIR}/target/bin
+# chmod +x target/bin/webdriver.sh
+
+# (target/bin/webdriver.sh "$TESTS_SCOPE" $@)
